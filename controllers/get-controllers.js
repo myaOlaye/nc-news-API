e7df758cbdb390fcef98c969c333c46d894f966f
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2024-11-25 13:21:30 +0000

    Add endpoint GET /api/articles/:article_id

--- controllers/get-controllers.js
+++ controllers/get-controllers.js
@@ -1,16 +1,23 @@
 const endpointsJson = require("../endpoints.json");
-const { selectTopics } = require("../models/get-models");
+const { selectTopics, selectArticle } = require("../models/get-models");
 
 exports.getApi = (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
 };
 
 exports.getTopics = (req, res) => {
-  selectTopics()
-    .then((topics) => {
-      res.status(200).send({ topics });
+  selectTopics().then((topics) => {
+    res.status(200).send({ topics });
+  });
+};
+
+exports.getArticle = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticle(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
     })
     .catch((err) => {
-      console.log(err);
+      next(err);
     });
 };
--- models/get-models.js
+++ models/get-models.js
@@ -5,3 +5,14 @@ exports.selectTopics = () => {
     return rows;
   });
 };
+
+exports.selectArticle = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      return rows[0];
+    });
+};
