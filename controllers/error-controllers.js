a9fbf830211aa03931d05023f769bd2d5da6bcb5
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2024-11-26 12:37:19 +0000

    Add endpoint POST /api/articles/:article_id/comments

--- controllers/error-controllers.js
+++ controllers/error-controllers.js
@@ -12,4 +12,8 @@ exports.postgressErrorHandler = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Bad request" });
   }
+
+  if (err.code === "23503") {
+    res.status(404).send({ msg: "Not found" });
+  }
 };
---
+++ controllers/post-controllers.js
@@ -0,0 +1,15 @@
+const { insertNewComment } = require("../models/post-models");
+
+exports.postComment = (req, res, next) => {
+  const newComment = req.body;
+  const { article_id } = req.params;
+
+  insertNewComment(newComment, article_id)
+    .then((newComment) => {
+      res.status(201).send(newComment);
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+};
