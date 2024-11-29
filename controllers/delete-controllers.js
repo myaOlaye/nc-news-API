b594de9131f09e85d04be4c6f2e8edc5abdc32ed
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2024-11-29 16:34:44 +0000

    Add endpoint DELETE /api/articles/:article_id

--- controllers/delete-controllers.js
+++ controllers/delete-controllers.js
@@ -1,7 +1,9 @@
 const {
   removeComment,
   checkCommentExists,
+  removeArticle,
 } = require("../models/delete-models");
+const { selectArticle } = require("../models/get-models");
 
 exports.deleteComment = (req, res, next) => {
   const { comment_id } = req.params;
@@ -12,6 +14,20 @@ exports.deleteComment = (req, res, next) => {
       res.status(204).send();
     })
     .catch((err) => {
+      next(err);
+    });
+};
+
+exports.deleteArticle = (req, res, next) => {
+  const { article_id } = req.params;
+  const promises = [selectArticle(article_id), removeArticle(article_id)];
+
+  Promise.all(promises)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((err) => {
+      console.log("here");
       next(err);
     });
 };
--- routes/articles-router.js
+++ routes/articles-router.js
@@ -8,9 +8,14 @@ const {
 } = require("../controllers/get-controllers");
 const { postComment, postArticle } = require("../controllers/post-controllers");
 const { patchArticle } = require("../controllers/patch-controllers");
+const { deleteArticle } = require("../controllers/delete-controllers");
 
 articlesRouter.route("/").get(getArticles).post(postArticle);
-articlesRouter.route("/:article_id").get(getArticle).patch(patchArticle);
+articlesRouter
+  .route("/:article_id")
+  .get(getArticle)
+  .patch(patchArticle)
+  .delete(deleteArticle);
 articlesRouter
   .route("/:article_id/comments")
   .get(getComments)
