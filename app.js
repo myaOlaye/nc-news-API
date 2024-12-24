346eb51a87db29cb0b50bcab0ca7bdc67a24725b
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2024-12-24 20:40:45 +0000

    specify allowed origins for CORS

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ const {
 
 const app = express();
 
-app.use(cors());
-app.options("*", cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
