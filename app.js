70dca494bf407ad75a856f9f542a04c7d03d1f17
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2025-01-05 21:02:04 +0000

    clanup

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.options("*", cors());
 
