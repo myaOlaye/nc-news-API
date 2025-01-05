1f1ec7d0460031ab8e4c2d4da3e0625caad721b2
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2025-01-05 19:28:50 +0000

    Adjusted refresh token cookie details

--- controllers/logout-controller.js
+++ controllers/logout-controller.js
@@ -13,7 +13,11 @@ exports.handleLogout = (req, res, next) => {
     .query("SELECT * FROM users WHERE refresh_token = $1", [refreshToken])
     .then(({ rows }) => {
       if (!rows[0]) {
-        res.clearCookie("jwt", { httpOnly: true, secure: true });
+        res.clearCookie("jwt", {
+          httpOnly: true,
+          secure: true,
+          sameSite: none,
+        });
         return res
           .status(204)
           .send({ msg: "Refresh token not in db, deleted" });
@@ -25,7 +29,11 @@ exports.handleLogout = (req, res, next) => {
           [refreshToken]
         )
         .then(() => {
-          res.clearCookie("jwt", { httpOnly: true, secure: true });
+          res.clearCookie("jwt", {
+            httpOnly: true,
+            secure: true,
+            sameSite: none,
+          });
           return res
             .status(204)
             .send({ msg: "Refresh token deleted and user logged out." });
